ced199e9da3ff07dcad09681305c53ba67bbe05e
JORDANA CLARIMUNDO DIONISIO <jordana.8904@aluno.pr.senac.br>
2022-12-05 11:48:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Página Inicial - Meus Games</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Página Inicial - Meus Games</h1>
+            <h3>Candy Crush</h3>
+            <p>Candy Crush é um jogo de puzzle lançado pela King em 12 de abril de 2012 para Facebook, em 6 de setembro de 2012 para Windows Phone, em 14 de novembro de 2012 para iOS, em 14 de dezembro de 2012 para Android, em 11 de setembro de 2014 para Fire OS e em 29 de julho de 2015 para Windows 10 e Tizen.</p>
+            <img src="imagens/candy.webp" alt="">
+        </div>
+    </body>
+</html>
